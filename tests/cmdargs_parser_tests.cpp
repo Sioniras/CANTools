d9db4f62cdaa24ff14d2dcff15b3eb54048a432b
Claus Nielsen <webmaster@nighthawk.dk>
2023-02-28 22:16:05 +0100

    Added interface name to cmdargs_parser.

--- tests/cmdargs_parser_tests.cpp
+++ tests/cmdargs_parser_tests.cpp
@@ -18,14 +18,43 @@ class cmdargs_parser
 	public:
 		enum class values
 		{
-			input_interface,
-			output_interface,
+			input_interface_type,
+			output_interface_type,
+			input_interface_name,
+			output_interface_name,
 		};
 
 	public:
-		cmdargs_parser(int argc, const char** argv) {}
+		cmdargs_parser(int argc, const char** argv)
+		{
+			for(int i = 0; i < argc; i++)
+			{
+				if(std::string(argv[i]).compare("--input") == 0)
+				{
+					if(i+2 < argc) _values[values::input_interface_name] = argv[i+2];
+					i += 2;
+				}
+				else if(std::string(argv[i]).compare("--output") == 0)
+				{
+					if(i+2 < argc) _values[values::output_interface_name] = argv[i+2];
+					i += 2;
+				}
+			}
+		}
 
-		std::string get(values parameter) { return "can"; }
+		std::string get(values parameter)
+		{
+			// Lookup the parameter and return it if found
+			auto v = _values.find(parameter);
+			if(v != _values.end())
+				return v->second;
+
+			return "can";
+		}
+		bool valid() const { return true; }
+
+	private:
+		std::map<values,std::string> _values;
 };
 
 // No "actual testing", but demonstrating how to construct cmdargs_parser
@@ -34,6 +63,8 @@ TEST(cmdargs_parser, construction_empty_cmdline_args)
 	const int argc = 1;
 	const char* argv[argc] { "cantool" };	// There will always be a first parameter with the name of the executable
 	cmdargs_parser parser{ argc, argv };
+
+	EXPECT_TRUE(parser.valid());
 }
 
 TEST(cmdargs_parser, specify_input_interface)
@@ -43,7 +74,35 @@ TEST(cmdargs_parser, specify_input_interface)
 	const char* argv[argc] { "cantool", "--input", input.c_str() };
 	cmdargs_parser parser{ argc, argv };
 
-	EXPECT_STREQ(parser.get(cmdargs_parser::values::input_interface).c_str(), input.c_str());
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::input_interface_type).c_str(), input.c_str());
+	EXPECT_TRUE(parser.valid());
+}
+
+TEST(cmdargs_parser, specify_input_interface_type_and_name)
+{
+	const std::string input = "can";
+	const std::string interface_name = "vcan0";
+	const int argc = 4;
+	const char* argv[argc] { "cantool", "--input", input.c_str(), interface_name.c_str() };
+	cmdargs_parser parser{ argc, argv };
+
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::input_interface_type).c_str(), input.c_str());
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::input_interface_name).c_str(), interface_name.c_str());
+	EXPECT_TRUE(parser.valid());
+}
+
+// Use a different interface name (i.e. it cannot be a hardcoded constant, as would be the first TDD behaviour)
+TEST(cmdargs_parser, specify_input_interface_type_and_name2)
+{
+	const std::string input = "can";
+	const std::string interface_name = "can1";
+	const int argc = 4;
+	const char* argv[argc] { "cantool", "--input", input.c_str(), interface_name.c_str() };
+	cmdargs_parser parser{ argc, argv };
+
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::input_interface_type).c_str(), input.c_str());
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::input_interface_name).c_str(), interface_name.c_str());
+	EXPECT_TRUE(parser.valid());
 }
 
 TEST(cmdargs_parser, specify_output_interface)
@@ -53,5 +112,33 @@ TEST(cmdargs_parser, specify_output_interface)
 	const char* argv[argc] { "cantool", "--output", output.c_str() };
 	cmdargs_parser parser{ argc, argv };
 
-	EXPECT_STREQ(parser.get(cmdargs_parser::values::output_interface).c_str(), output.c_str());
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::output_interface_type).c_str(), output.c_str());
+	EXPECT_TRUE(parser.valid());
+}
+
+TEST(cmdargs_parser, specify_output_interface_type_and_name)
+{
+	const std::string output = "can";
+	const std::string interface_name = "vcan0";
+	const int argc = 4;
+	const char* argv[argc] { "cantool", "--output", output.c_str(), interface_name.c_str() };
+	cmdargs_parser parser{ argc, argv };
+
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::output_interface_type).c_str(), output.c_str());
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::output_interface_name).c_str(), interface_name.c_str());
+	EXPECT_TRUE(parser.valid());
+}
+
+// Use a different interface name (i.e. it cannot be a hardcoded constant, as would be the first TDD behaviour)
+TEST(cmdargs_parser, specify_output_interface_type_and_name2)
+{
+	const std::string output = "can";
+	const std::string interface_name = "can1";
+	const int argc = 4;
+	const char* argv[argc] { "cantool", "--output", output.c_str(), interface_name.c_str() };
+	cmdargs_parser parser{ argc, argv };
+
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::output_interface_type).c_str(), output.c_str());
+	EXPECT_STREQ(parser.get(cmdargs_parser::values::output_interface_name).c_str(), interface_name.c_str());
+	EXPECT_TRUE(parser.valid());
 }
